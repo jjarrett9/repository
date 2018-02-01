106f25452625fe5b5bd25a85c37aca30643af43f
mitchveazey <mitchveazey@gmail.com>
2018-02-01 16:55:16 -0500

    return of my p2 files that were DESTROYED by the JOHN, THE GOD OF DESTRUCTION

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -3,7 +3,7 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
  * @version 1.1
@@ -31,14 +31,28 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
-	}
+      int max = input.length();
+      char[] arr = new char[max];
+      String result = "";
+
+      for (int i = 0; i < input.length(); i++ ) {
+          Random randNum = new Random();
+          int numSpot = randNum.nextInt(max);
+          arr[numSpot] = input.charAt(i);
+      }
+
+      for (int i = 0; i < arr.length; i++) {
+          result += arr[0];
+      }
+      return result;
+  }
+  
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
